58f266741c0eaecafe652788b535e72e82303873
rkyzk <reikoyazaki@yahoo.com>
2022-09-28 12:25:57 +0000

    Add basic structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore () {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
